12e84d86a8f2edaf859c197546977fe78fa9bcaa
Eric Farley <ericfarley96@gmail.com>
2022-07-13 20:43:02 -0300

    Added bounce limit on top and bottom

--- taca-taca/game/scripting/collide_borders_action.py
+++ taca-taca/game/scripting/collide_borders_action.py
@@ -34,15 +34,3 @@ class CollideBordersAction(Action):
         elif y >= (FIELD_BOTTOM - BALL_WIDTH):
             ball.bounce_y()
             self._audio_service.play_sound(bounce_sound)
-
-        teams = cast.get_actors(TEAM_GROUP)
-        for team in teams:
-            team_body = team.get_body()
-            team_position = team_body.get_position()
-               
-            if team_position.get_y() < FIELD_TOP:
-                team.swing_down()
-                print(team_position.get_y())
-
-            elif team_position.get_y() >= (FIELD_BOTTOM - BALL_WIDTH):
-                team.swing_up()
